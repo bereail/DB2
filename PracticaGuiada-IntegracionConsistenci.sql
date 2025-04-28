7e64a2875c3ec9187bf6937d6c7ae307369d655b
Berenice Solohaga <bsolohaga@hep.local>
2025-04-28 10:44:59 -0300

    practica guiada

--- PracticaGuiada-IntegracionConsistenci.sql
+++ PracticaGuiada-IntegracionConsistenci.sql
@@ -2,48 +2,52 @@ create database SistemaGestionVentasDB;
 
 use SistemaGestionVentasDB;
 
-create table Clientes(
-	id_cliente int auto_increment primary key,
-	nombre varchar(50) not null,
-    correo_electronico varchar(100) not null unique
+CREATE TABLE Clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    correo_electronico VARCHAR(100) NOT NULL UNIQUE,
+    CONSTRAINT chk_correo CHECK (correo_electronico REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
 );
 
-create table Productos(
-	id_producto int auto_increment primary key,
-    nombre varchar(50) not null,
-    precio decimal(10, 2) not null,
-    stock int default 0,
-    check (precio > 0),
-    check (stock >= 0)
+CREATE TABLE Productos (
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    stock INT DEFAULT 0,
+    CONSTRAINT chk_precio_positivo CHECK (precio > 0),
+    CONSTRAINT chk_stock_no_negativo CHECK (stock >= 0)
 );
 
-create table Pedidos(
-	id_pedidos int auto_increment primary key,
-    id_cliente int not null,
-    id_producto int not null,
-    cantidad int not null check(cantidad > 0),
-    fecha date not null,
-	FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente) on delete cascade,
-    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) on delete cascade
+
+CREATE TABLE Pedidos (
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_producto INT NOT NULL,
+    cantidad INT NOT NULL CHECK (cantidad > 0),
+    fecha DATE NOT NULL,
+    CONSTRAINT fk_pedido_cliente FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente) ON DELETE CASCADE,
+    CONSTRAINT fk_pedido_producto FOREIGN KEY (id_producto) REFERENCES Productos(id_producto) ON DELETE CASCADE
 );
 
 
 DELIMITER //
+
 CREATE TRIGGER valida_stock_pedido
-before insert on pedidos
-for each row
-begin 
-	declare stock_disponible int;
+BEFORE INSERT ON Pedidos
+FOR EACH ROW
+BEGIN 
+    DECLARE stock_disponible INT;
     
-    select stock into stock_disponible
-    from productos
-    where id_producto = new.id_producto;
+    SELECT stock INTO stock_disponible
+    FROM Productos
+    WHERE id_producto = NEW.id_producto;
     
-    if new.cantidad > stock_disponible then
-		signal sqlstate '45000'
-        set message_text = 'no hay suficiente stock para relizar el pedido';
-	end if;
-end;
+    IF NEW.cantidad > stock_disponible THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'No hay suficiente stock para realizar el pedido';
+    END IF;
+END;
+
 //
 
 DELIMITER ;
